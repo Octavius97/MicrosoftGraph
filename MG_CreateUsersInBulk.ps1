9a53a38b29bf78c073fbd1064b5b90dfe2c23380
Octavius97 <31940044+Octavius97@users.noreply.github.com>
2024-02-11 15:20:37 -0600

    Update MG_CreateUsersInBulk.ps1

--- MG_CreateUsersInBulk.ps1
+++ MG_CreateUsersInBulk.ps1
@@ -1,5 +1,15 @@
-#Conectarse a Graph usando los permisos necesarios
+# Conectarse a Graph usando los permisos necesarios
 Connect-MgGraph -Scopes User.ReadWrite.All
 
-#Importar los datos desde un CSV
+# Importar los datos desde un CSV
+# Se asume que el csv contiene los siguiente headers (ninguno vacío)
+# UPN | DisplayName | FirstName | LastName | Job | Department | Password 
 $users = Import-CSV -Path ".\csv_Path"
+
+# Recorrer cada fila para crear el usuario
+foreach($u in $users){
+    New-MgUser -UserPrincipalName $u.UPN -MailNickName $u.UPN.Split('@')[0] -GivenName $u.FirstName -SurName $u.LastName -JobTitle $u.Job -Departmen $u.Department -PasswordProfile @{
+        ForceChangePasswordNextSignIn = $true
+        Password = $u.Password
+    } -AccountEnabled
+}
