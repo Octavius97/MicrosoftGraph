4b2d54b9348e0c3c940e7a283ce63cc95f3e74bb
Octavius97 <31940044+Octavius97@users.noreply.github.com>
2024-03-27 13:27:35 -0600

    Add

--- Listar Usuarios con Licencia Especifica.ps1
+++ Listar Usuarios con Licencia Especifica.ps1
@@ -4,6 +4,4 @@ $users = Get-MgBetaUser -All
 
 $PB = Get-MgBetaSubscribedSku | Where-object SkuPartNumber -EQ ""
 
-$users | Select-Object DisplayName, UserPrincipalName, EmployeeId -ExpandProperty AssignedLicenses `
-| Select-Object -ExcludeProperty AdditionalProperties, DisabledPlans
-| Where-Object SkuId -EQ $PB.SkuId | Export-Csv -Path C:\report.csv
+$users | Select-Object DisplayName, UserPrincipalName, EmployeeId -ExpandProperty AssignedLicenses | Select-Object -ExcludeProperty AdditionalProperties, DisabledPlans | Where-Object SkuId -EQ $PB.SkuId | Export-Csv -Path C:\report.csv
