148434de186929111cb572b831ecbebd597f25a4
Octavio Morales <octaviomorales97@gmail.com>
2024-04-16 10:44:27 -0600

    Updates module connection

---
+++ Modules_Connections.ps1
@@ -0,0 +1,59 @@
+# Inicio del Modulo
+
+$opcion = $null
+while ($opcion -notmatch "^[1-6]$") {
+    Write-Host "Seleccione el modulo a conectarse"
+    Write-Host "1. Microsoft Graph"
+    Write-Host "2. Azure AD Graph"
+    Write-Host "3. Microsoft Online Services"
+    Write-Host "4. Exchange Online"
+    Write-Host "5. SharePoint Online"
+    Write-Host "6. Cerrar"
+    $opcion = Read-Host "Seleccione una opcion"
+    if($opcion -notmatch "^[1-6]$") {
+        [System.Console]::ForegroundColor = "Red"
+        Write-Host "Opcion no valida"
+        [System.Console]::WriteLine()
+        [System.Console]::ResetColor()
+    }
+}
+
+switch ($opcion) {
+    1 {
+        Import-Module Microsoft.Graph
+        Connect-MgGraph -Scopes "User.Read.All", "Group.Read.All", "Directory.Read.All", "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All"
+    }
+    2 {
+        Import-Module AzureAD
+        $username = Read-Host "Ingrese el nombre del usuario: "
+        $password = Read-Host "Ingrese la contraseña: " -AsSecureString
+        $credential = New-Object System.Management.Automation.PSCredential ($username, $password)
+        Connect-AzureAD -Credential $credential
+    }
+    3 {
+        Import-Module MSOnline
+        $username = Read-Host "Ingrese el nombre del usuario: "
+        $password = Read-Host "Ingrese la contraseña: " -AsSecureString
+        $credential = New-Object System.Management.Automation.PSCredential ($username, $password)
+        Connect-MsolService -Credential $credential
+    }
+    4 {
+        import-module ExchangeOnlineManagement
+        $username = Read-Host "Ingrese el nombre del usuario: "
+        $password = Read-Host "Ingrese la contraseña: " -AsSecureString
+        $credential = New-Object System.Management.Automation.PSCredential ($username, $password)
+        Connect-ExchangeOnline -Credential $credential
+    }
+    5 {
+        Import-Module Microsoft.Online.SharePoint.PowerShell
+        $username = Read-Host "Ingrese el nombre del usuario: "
+        $password = Read-Host "Ingrese la contraseña: " -AsSecureString
+        $credential = New-Object System.Management.Automation.PSCredential ($username, $password)
+        $tenant = Read-Host "Ingrese el nombre del tenant: "
+        Connect-SPOService -Url "https://$tenant-admin.sharepoint.com" -Credential $credential
+    }
+    6 {
+        Write-Host "Bye"
+        exit
+    }
+}
