18f6cfa86d3119b4803e63055c3c068f4540db26
Octavio Morales <octaviomorales97@gmail.com>
2025-06-24 16:16:08 +0000

    Add options to retrieve the per-user MFA state

--- User Administration/Manage Per-user MFA with Graph.ps1
+++ User Administration/Manage Per-user MFA with Graph.ps1
@@ -10,6 +10,29 @@ Connect-MgGraph -scopes Policy.ReadWrite.AuthenticationMethod, User.ReadWrite.Al
 $user = Get-MgUser -UserId "userName@domain.com"
 Invoke-MgGraphRequest -Method GET -URI "beta/users/$($user.Id)/authentication/requirements"
 
+# Get the all users MFA state
+Get-MgUser -All | Foreach-Object {
+  $user = $_
+  $userMFAstate = Invoke-MgGraphRequest -Method GET -URI "beta/users/$($user.Id)/authentication/requirements"
+  [PSCustomObject]@{
+    UserPrincipalName = $user.UserPrincipalName
+    DisplayName = $user.DisplayName
+    "MFA State" = $userMFAstate.perUserMfaState
+  }
+}
+
 # Set the user MFA state (enabled, disabled, enforced)
 $body = @{ "perUserMfaState" = "enabled" }
 Invoke-MgGraphRequest -Method POST -URI "beta/users/$($user.Id)/authentication/requirements" -Body $body
+
+<#
+  For multiple users in a CSV file
+#>
+
+# Get the users from the CSV file (Header with UPN)
+Import-CSV -Path "C:\temp\user.csv" | Foreach-Object {
+  $user = Get-MgUser -UserId $_.UPN
+  # Set the user MFA state (enabled, disabled, enforced)
+  $body = @{ "perUserMfaState" = "enabled" }
+  Invoke-MgGraphRequest -Method POST -URI "beta/users/$($user.Id)/authentication/requirements" -Body $body
+}
